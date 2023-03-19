ca6262e6b71639e3b6efcbe3696eb7e6aaea5464
sknabirasool <sknabirasool104@gmail.com>
2023-03-19 16:25:01 +0530

    save files

---
+++ day0/Employee.java
@@ -0,0 +1,23 @@
+import  java.io.*;
+public  class  Employee{
+
+public String name;
+public double salary;
+public  Employee  (String  empName){
+name  =  empName;
+}
+public  void  setSalary(double  empSal){
+salary  =  empSal;
+}
+public  void  printEmp(){
+System.out.println("name	:  "  +  name); 
+System.out.println("salary  :"  +  salary);
+}
+public  static  void  main(String  args[])
+{ 
+Employee  empOne  =  new  Employee("Ransika"); 
+empOne.setSalary(1000);
+empOne.printEmp();
+}
+}
+
---
+++ day0/Test1.java
@@ -0,0 +1,11 @@
+import  java.io.*; 
+public  class  Test1{
+//  salary	variable  is  a  private  static  variable private  static  double  salary;
+
+//  DEPARTMENT  is  a  constant
+public  static  final  String  DEPARTMENT  =  "Development  "; 
+public  static  void  main(String  args[]){
+int salary  =  1000;
+System.out.println(DEPARTMENT  +  "average  salary:"  +  salary);
+}
+}
