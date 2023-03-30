748c07399dfbfa347808c9b25604dce7b1edadab
sknabirasool <sknabirasool104@gmail.com>
2023-03-30 19:41:42 +0530

    save

--- day3/Cal.java
+++ day3/Cal.java
@@ -1,18 +1,19 @@
 public class Cal{
 
-    String name="calculater";
+    public int c=2000;
     public int add()
     {
         int a=0;//localvariables
         int c=a+2000;
         return c;
     }
+    
     public static void main(String args[])
     {
-        int a=0;
-        System.out.println(a);
+       
         Cal obj=new Cal();
         System.out.println(obj.add());
-        System.out.println("name is "+obj.name);
+        System.out.println(obj.add());//2000
+   
     }
 }
---
+++ day4-variables/TestDemo.java
@@ -0,0 +1,9 @@
+public class TestDemo
+{
+    public static String collegeName="JNTUA";
+
+    public static void main(String args[])
+    {
+        System.out.println("College Name Is"+collegeName);
+    }
+}
